8e8db85dc18e71ba410057ca90303143c524a6f6
Jesse Ruhland <jesse.ruhland@gmail.com>
2022-08-09 15:07:48 -0400

    assignment complete

---
+++ app.js
@@ -0,0 +1,20 @@
+const PI = 3.14
+
+/*
+1) What is the difference between var and let?
+var can be reassigned, redeclared, mutated, and has function scope.
+let can be reassigned, mutated, and has block scope.  let cannot be redeclared.
+
+2) What is the difference between var and const?
+var can be reassigned, redeclared, mutated, and has function scope.
+const can be mutated and has block scope.  const cannot be reassigned or redeclared.
+
+3) What is the difference between let and const?
+let can be reassigned, mutated, and has block scope.  let cannot be redeclared.
+const can be mutated and has block scope.  const cannot be reassigned or redeclared.
+
+4) What is hoisting?
+Hoisting is when var declarations are read before anything else in the code and result in variables that are 'undefined' until initialized.
+
+*/
+
